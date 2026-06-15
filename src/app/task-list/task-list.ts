c25a9373ab9a5b9ac3eed7d5bc61001e74ef39f1
MazingoM <mazingombatha@gmail.com>
2026-06-15 12:42:48 +0200

    the task is created

--- src/app/task-list/task-list.ts
+++ src/app/task-list/task-list.ts
@@ -1,30 +1,16 @@
-import { Component, signal,WritableSignal } from '@angular/core';
-import { Task } from '../task.model';
-import {v4 as uuid} from 'uuid';
-import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { TaskService } from '../task-service';
 
 
 @Component({
   selector: 'app-task-list',
-  imports: [DatePipe],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css',
 })
 export class TaskList {
 
-    tasks: WritableSignal<Task[]> = signal([
-    {
-      id: uuid(),
-      title: "Task 1",
-      description: "This is the first task",
-      createdAt: new Date()
-    },
-    {
-      id: uuid(),
-      title: "Task 2",
-      description: "This is the second task",
-      createdAt: new Date()
-    
-    }
-  ]);
+  private taskService = inject(TaskService);
+
+  tasks = this.taskService.tasks;
 }
